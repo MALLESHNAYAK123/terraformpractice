73135120515e08aa5d35041cecf3b92245c5ac90
MALLESHNAYAK123 <chinnuhoney130@gmail.com>
2025-04-11 03:05:42 +0530

    second commit

--- main.tf
+++ main.tf
@@ -11,18 +11,20 @@ resource "aws_vpc" "my-vpc" {
 #subnets
 
 resource "aws_subnet" "public-subnet-1" {
-  vpc_id            = aws_vpc.my-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "pub-sub-1"
   }
 }
 
 resource "aws_subnet" "public-subnet-2" {
-  vpc_id            = aws_vpc.my-vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-south-1b"
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
   tags = {
     Name = "pub-sub-2"
   }
@@ -44,4 +46,68 @@ resource "aws_subnet" "private-subnet-2" {
   tags = {
     Name = "pvt-sub-2"
   }
+}
+
+#internetgateway
+
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "my-internet-gw"
+  }
+
+}
+
+resource "aws_eip" "my-eip" {
+
+}
+
+#natgateway
+
+resource "aws_nat_gateway" "natgateway" {
+  allocation_id = aws_eip.my-eip.id
+  subnet_id     = aws_subnet.public-subnet-1.id
+  tags = {
+    Name = "nat-gw"
+  }
+}
+
+#route table
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-igw.id
+  }
+  tags = {
+    Name = "my-pub-route"
+  }
+
+}
+
+resource "aws_route_table" "pvt-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natgateway.id
+  }
+  tags = {
+    Name = "my-pvt-route"
+  }
+
+}
+
+#rt association
+
+resource "aws_route_table_association" "pub-rt-asso" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-rt.id
+
+}
+
+resource "aws_route_table_association" "pvt-rt-asso" {
+  subnet_id      = aws_subnet.private-subnet-1.id
+  route_table_id = aws_route_table.pvt-rt.id
+
 }
